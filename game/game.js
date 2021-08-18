cc708da489cfc3a0dd92f53d51bbc9065e300af6
Esmaelhbr <esmael7@live.com>
2021-08-18 13:45:55 -0400

    html has been updated, added the game props to the game.js

---
+++ game/game.js
@@ -0,0 +1,112 @@
+
+
+let simpleLevelPlan = `
+......................
+..#................#..
+..#..............=.#..
+..#.........o.o....#..
+..#.@......#####...#..
+..#####............#..
+......#++++++++++++#..
+......##############..
+......................`;
+
+//stores a level object
+
+class Level{
+	constructor(plan){
+
+		let rows = plan.trim().split("\n").map(l => [...l]); 
+		this.hight = rows.lenght;
+		this.width= rows[0].lenght;
+		this.startActors=[];
+
+		this.rows = rows.map((row,y) =>{
+			return row.map((ch,x) =>{
+				let type = levelChars[ch];
+				if(typeof type =="string") return type;
+				this.startActors.push(
+					type.create(new Vec(x,y), ch));
+				return "empty";
+					
+			})
+		})
+
+	}
+}
+
+//trach the state of a running game
+
+class State{
+	constructor(level,actors,status){
+		this.level =level;
+		this.actors = actors;
+		this.status = status;
+	}
+	static start(level){
+		return new State(level,level.startActors,"playing");
+
+	}
+	get player(){
+		return this.actors.find(a => a.type=="player")
+	}
+}
+
+
+//two dimensional values, position and size
+
+class Vec{
+	constructor(x,y){
+		this.x =x;
+		this.y=y;
+
+	}
+	plus(other){
+		return new Vec(this.x + other.x,this.y + other.y);
+	}
+	times(factor){
+		return new Vec(this.x * factor, this.y * factor);
+
+	}
+}
+
+//player class
+
+class Player{
+	constructor(pos,speed){
+		this.pos =pos;
+		this.speed = speed;
+	}
+	get type(){
+		return "player";
+	}
+	static create(pos){
+		return new player(pos.plus(new Vec(0, -0.5)), new Vec(0, 0));
+	}
+}
+
+Player.prototype.size= new Vec(0.8, 1.5);
+
+
+class Lava{
+	constructor(pos,speed,reset){
+		this.pos = pos;
+		this.speed = speed;
+		this.reset = reset
+	}
+	get type(){
+		return "lava";
+	}
+	static create(pos, ch){
+		if(ch=="="){
+			return new Lava(pos,new Vec(2,0));
+		}else if(ch =="|"){
+			return new Lava(pos,new Vec(0, 2))
+
+		}else if(ch == "v"){
+			return new Lava(pos,new Vec(0,3),pos);
+		}
+	}
+}
+
+Lava.prototype.size = new Vec(1,1);
