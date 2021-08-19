b2f9f699e62bb7cbd5d06a8b62ed6e9b06f961f1
Esmaelhbr <esmael7@live.com>
2021-08-18 20:44:42 -0400

    game props has been updated

--- game/game.js
+++ game/game.js
@@ -81,7 +81,7 @@ class Player{
 		return "player";
 	}
 	static create(pos){
-		return new player(pos.plus(new Vec(0, -0.5)), new Vec(0, 0));
+		return new Player(pos.plus(new Vec(0, -0.5)), new Vec(0, 0));
 	}
 }
 
@@ -110,3 +110,31 @@ class Lava{
 }
 
 Lava.prototype.size = new Vec(1,1);
+
+class Coin{
+	constructor(pos,basePos,wobble){
+		this.pos = pos;
+		this.basePos = basePos;
+		this.wobble = wobble;
+	}
+	get type(){
+		return "coin";
+	}
+	static create(pos){
+		let basePos = pos.plus(new Vec(0.2,0.1))
+		return new Coin(basePos, basePos,
+					Math.random() * Math.PI * 2);
+	}
+
+}
+
+Coin.prototype.size= new Vec(0.6,0.6);
+
+const levelChars = {
+  ".": "empty", "#": "wall", "+": "lava",
+  "@": Player, "o": Coin,
+  "=": Lava, "|": Lava, "v": Lava
+};
+
+let simpleLevel = new Level(simpleLevelPlan);
+console.log(`${simpleLevel.width} by ${simpleLevel.height}`);
